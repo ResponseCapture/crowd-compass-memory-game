b7539f75ec7709858424a810b89553ac763e35b3
Bryant Syme <prefu@hotmail.com>
2015-09-30 22:39:00 -0700

    refactor check method

--- assets/js/components/memorygame.js
+++ assets/js/components/memorygame.js
@@ -33,8 +33,10 @@ function Game(cards, timeout, interval) {
   var timer,
     begin,
     timer,
-    limit = 45,
-    limitMs = limit * 1000, 
+    limit = 10,
+    critical = 5,
+    limitMs = limit * 1000,
+    noop = function () {},
     game = {};
 
   game.deck = shuffle(cards.concat(cards))
@@ -45,56 +47,8 @@ function Game(cards, timeout, interval) {
       };
     });
 
-  var start = function () {
-    begin = new Date();
-    mprogress.start();
-
-    return interval(function () {
-      var elapsed = game.elapsedMs() / 1000;
-
-      game.isCritical = elapsed <= 5;
-      mprogress.set((game.remaining() || 0) / limitMs);
-
-      if (elapsed >= limit) {
-        stop();
-      }
-    }, 50);
-  };
-
-  var stop = function () {
-    var end = new Date() - begin;
-    interval.cancel(timer);
-    var faceDown = game.deck
-      .filter(function (c) { 
-        return !c.isFaceUp; 
-      }).length;
-
-      timeout(function() {
-        game.results = {
-          time: Math.ceil(end / 1000),
-          total: game.deck.length / 2,
-          missing: Math.ceil(faceDown / 2)
-        };
-        mprogress.end();
-      }, 1500);
-  };
-
-  game.elapsedMs = function () {
-    return new Date() - begin;
-  }
-
-  game.remaining = function () {
-    if (begin && game.elapsedMs() < limitMs) {
-      return limitMs - game.elapsedMs();
-    }
-  }
-
   var checkNext;
-  game.check = function(card) {
-    if (!timer) {
-      timer = start();
-    }
-      
+  var check = function(card) {
     if (checkNext) {
       checkNext(card);
     } else {
@@ -117,6 +71,60 @@ function Game(cards, timeout, interval) {
         }
       }
     }
+  };
+
+  var start = function () {
+    game.check = check;
+    begin = new Date();
+    mprogress.start();
+
+    timer = interval(function () {
+      var elapsed = game.elapsedMs() / 1000,
+        remaining = game.remaining();
+
+      game.isCritical = Math.round(remaining / 1000) <= critical;
+      mprogress.set((remaining || 0) / limitMs);
+      console.log((remaining || 0) / limitMs);
+
+      if (elapsed >= limit) {
+        stop();
+      }
+    }, 50);
+  };
+
+  var stop = function () {
+    interval.cancel(timer);
+    game.check = noop;
+    var end = new Date() - begin;
+    var faceDown = game.deck
+      .filter(function (c) { 
+        return !c.isFaceUp; 
+      }).length;
+
+      timeout(function() {
+        game.results = {
+          time: Math.ceil(end / 1000),
+          total: game.deck.length / 2,
+          missing: Math.ceil(faceDown / 2)
+        };
+        mprogress.end();
+      }, 1500);
+  };
+
+  game.elapsedMs = function () {
+    return new Date() - begin;
+  }
+
+  game.remaining = function () {
+    if (begin && !game.results) {
+      var elapsed = game.elapsedMs();
+      return elapsed < limitMs ? limitMs - elapsed : 0;
+    }
+  }
+
+  game.check = function (card) {
+    start();
+    check(card);
   };
 
   return game;
